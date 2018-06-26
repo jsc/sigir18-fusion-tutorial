855a626be96104871de8f0f5fd07a3b5156d568d
J. Shane Culpepper <shane.culpepper@rmit.edu.au>
2018-06-26 22:03:57 +1000

    Add UQV tester.

---
+++ experiments/uqv/uqv_test.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+PF="../../bin/polyfuse"
+TE="../../bin/trec_eval"
+
+rm -rf BM25 FDM DFRee DLH13 ALL
+mkdir -p BM25 FDM DFRee DLH13 ALL
+
+# Get test runs ready
+echo "Copy runs and uncompress"
+cp ../../data/robust04-new/terrier-DFRee_Bo1bfree.uqv.run.xz .
+cp ../../data/robust04-new/terrier-DLH13_Bo1bfree.uqv.run.xz .
+cp ../../data/robust04-new/indri-uqv.bm25.qe.run.xz .
+cp ../../data/robust04-new/indri-uqv.fdm.qe.run.xz .
+xz -d *.xz
+
+echo "Fuse BM25 runs"
+./uqvfuse_all.sh indri-uqv.bm25.qe.run
+echo "Compute MAP for BM25 runs"
+for f in `ls out*.run`
+do 
+  $TE -m map ../../data/rob04.qrels $f > $f.eval
+done
+mv out.* BM25 
+
+echo "Fuse FDM runs"
+./uqvfuse_all.sh indri-uqv.fdm.qe.run
+echo "Compute MAP for FDM runs"
+for f in `ls out*.run`
+do 
+  $TE -m map ../../data/rob04.qrels $f > $f.eval
+done
+mv out.* FDM 
+
+echo "Fuse DLH13 runs"
+./uqvfuse_all.sh terrier-DLH13_Bo1bfree.uqv.run
+echo "Compute MAP for DLH12 runs"
+for f in `ls out*.run`
+do 
+  $TE -m map ../../data/rob04.qrels $f > $f.eval
+done
+mv out.* DLH13 
+
+echo "Fuse DFRee runs"
+./uqvfuse_all.sh terrier-DFRee_Bo1bfree.uqv.run
+echo "Compute MAP for DFree runs"
+for f in `ls out*.run`
+do 
+  $TE -m map ../../data/rob04.qrels $f > $f.eval
+done
+mv out.* DFree
+
+echo "Fuse All runs"
+./uqvfuse_all.sh *.run
+echo "Compute MAP for All runs"
+for f in `ls out*.run`
+do 
+  $TE -m map ../../data/rob04.qrels $f > $f.eval
+done
+mv out.* ALL 
+
+echo "Dump scores and sort"
+find . -name *.eval | xargs grep map *.eval | sort -n -k3 > sorted.txt
+
---
+++ experiments/uqv/uqvfuse_all.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# uqv$PF.sh: accepts many UQV files to fuse.
+# Export POLYPARAM variable with the $PF parameters you would like.
+
+#POLYPARAM="rrf -k 60 -d 1000"
+#POLYPARAM="combsum -n minmax -d 1000"
+#POLYPARAM="rbc -p 0.995"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PF="../../bin/polyfuse"
+TE="../../bin/trec_eval"
+
+rm -rf $DIR/tmp
+mkdir -p $DIR/tmp
+
+# We want to ensure each file has a unique identifier
+FCOUNTER=1
+
+# Build nums file. If you have different topics for each UQV file, you
+# absolutely MUST comment this out.
+cut -d"-" -f1 $1 | sort | uniq > $DIR/nums
+
+for uqvfile in "$@"
+do
+    for topic in $(cat $DIR/nums)
+    do
+        mkdir -p $DIR/tmp/$topic
+    done
+
+    # We want this to bucket the topic in the correct dir
+    # And, we also want it to make sure it's not conflicting with the same ID
+    # Which might happen if we were fusing different systems in here as well.
+    echo "$DIR"
+    awk -v fcounter=$FCOUNTER -v dir=$DIR '{ split($1, topic, "-"); print topic[1]" "$2" "$3" "$4" "$5" "$6 > dir"/tmp/"topic[1]"/"$1"-"fcounter; }' $uqvfile
+    FCOUNTER=$(($FCOUNTER + 1))
+done
+
+rm -rf $DIR/out.*
+
+#for folder in $DIR/tmp/*
+#do
+#    $DIR/$PF $POLYPARAM $folder/* >> $DIR/out.txt
+#done
+
+for m in combsum combmnz combanz combmin combmax combmed
+do
+  for folder in $DIR/tmp/*
+  do
+    $PF $m $folder/* >> out.$m.none.run
+    for n in sum std minmax minsum
+    do
+      $PF $m -n $n $folder/* >> out.$m.$n.run
+    done
+  done
+done
+
+for m in borda rrf isr logisr
+do
+  for folder in $DIR/tmp/*
+  do
+    $PF $m $folder/* >> out.$m.none.run
+  done
+done
+
+m=rbc
+for p in 0.5 0.8 0.95 0.99 
+do
+  for folder in $DIR/tmp/*
+  do
+    $PF $m -p $p $folder/* > out.$m-$p.none.run
+  done
+done
