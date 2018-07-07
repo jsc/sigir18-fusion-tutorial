d5db552bdfd597b763fdd73f4f38d1551b5f0660
J. Shane Culpepper <shane.culpepper@gmail.com>
2018-07-07 07:06:39 -0400

    Interactive show.

---
+++ experiments/risk/show_bonferroni.sh
@@ -0,0 +1,9 @@
+echo "System Mapping"
+echo "SysA = BM25"
+echo "SysB = FDM+QE"
+echo "SysC = TREC Best"
+echo "SysD = BM25 UQV"
+echo "SysE = BM25 UQV + FDM UQV + FDM-QE + RRF"
+echo "SysF = Terrier DLH13+QE UQV"
+cat result.txt
+
---
+++ experiments/risk/show_risk_analysis.sh
@@ -0,0 +1,11 @@
+echo "BM25 vs FDM T-Risk"
+cat trisk.fdm.txt
+read -rsp $'Press any key to continue...\n' -n1 key
+echo ""
+echo "BM25 vs FDM-QE T-Risk"
+cat trisk.qe.txt
+read -rsp $'Press any key to continue...\n' -n1 key
+echo ""
+echo "BM25 vs BM25 UQV T-Risk"
+cat trisk.bm25.txt
+read -rsp $'Press any key to continue...\n' -n1 key
